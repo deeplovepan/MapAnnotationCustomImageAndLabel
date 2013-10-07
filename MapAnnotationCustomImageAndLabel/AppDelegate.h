0cb1357f542a16ade0b99ada0b620404a3102679
Peter Pan <PeterPan@Peters-MacBook-Pro.local>
2013-10-07 21:38:44 +0800

    Initial Commit

---
+++ MapAnnotationCustomImageAndLabel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapAnnotationCustomImageAndLabel
+//
+//  Created by Peter Pan on 10/7/13.
+//  Copyright (c) 2013 Peter Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapAnnotationCustomImageAndLabelTests/MapAnnotationCustomImageAndLabelTests.m
@@ -0,0 +1,34 @@
+//
+//  MapAnnotationCustomImageAndLabelTests.m
+//  MapAnnotationCustomImageAndLabelTests
+//
+//  Created by Peter Pan on 10/7/13.
+//  Copyright (c) 2013 Peter Pan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MapAnnotationCustomImageAndLabelTests : XCTestCase
+
+@end
+
+@implementation MapAnnotationCustomImageAndLabelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
